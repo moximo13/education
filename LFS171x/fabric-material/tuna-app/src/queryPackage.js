8c2f9525bb73b5286c57d6317d50a2035265f7e7
fettpath <tongmaco@gmail.com>
2018-01-25 12:52:33 +0700

    commit queryPackage func: change variable

--- LFS171x/fabric-material/tuna-app/src/queryPackage.js
+++ LFS171x/fabric-material/tuna-app/src/queryPackage.js
@@ -15,7 +15,7 @@ var os = require('os');
 
 var fabric_client = new Fabric_Client();
 
-var key = req.params.id
+var receiver_loca = req.params.id
 
 // setup the fabric network
 var channel = fabric_client.newChannel('mychannel');
